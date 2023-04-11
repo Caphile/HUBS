e09f36ff72d960c070406ce1deeb582f58af206e
김희찬 <caphile98@gmail.com>
2023-04-11 20:57:39 +0900

    기초 정규화 작업

--- normalize.py
+++ normalize.py
@@ -4,39 +4,58 @@ import nltk
 from nltk.corpus import stopwords
 import re
 
-def stripSCharacter(text):  # 특수문자 제거
-    stripped_text = re.sub('[^a-zA-Z0-9\s]', '', text)
-    return stripped_text
+def openFile(fileName):
+    with open(fileName, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+def saveFile(fileName, text):
+    with open(fileName, 'w') as f:
+        for line in text:
+            f.write(line + '\n')
 
 def addStopwords(): # 불용어 커스텀
-    file = open('./Stopwords.txt', 'r')
-    
-    ASW = []    # additional stopwords
-    while 1:
-        word = file.readline().strip('\n')
-        if word == '':
-            break
-        ASW.append(word)
-    file.close()
+    words = openFile('./Stopwords.txt')
 
+    ASW = []    # additional stopwords
+    for word in words:
+        ASW.append(word)
     return ASW
 
-def removeStopword(SW):
-    pass
+class Normalize:    # 정규화 함수
+    def __init__(self, text, stopwords):
+        self.text = self.stripSCharacter(text)
+        self.text = self.removeStopword(self.text, stopwords)
+        self.text = self.lowercase(self.text)
 
+    def stripSCharacter(self, text):        # 특수문자 제거
+        return re.sub('[^a-zA-Z0-9\s]', '', text)
 
+    def removeStopword(self, text, SW):     # 불용어 제거
+        words = text.split(' ')
+        return ' '.join([word for word in words if word not in SW])
+
+    def lowercase(self, text):              # 소문자화
+        words = text.split(' ')
+        return ' '.join([word.lower() for word in words])
 
 nltk.download('stopwords')
+stopword_list = stopwords.words('english')
+stopword_list.append(addStopwords())
 
-stopwords = stopwords.words('english')
-stopwords.append(addStopwords())
+while 1:
+    fName = 'script_en.txt'
+    text = openFile(fName)
 
+    newText = []
+    for line in text:
+        if len(line) > 2 and line[-1 : ] == '\n':   # 텍스트 마지막의 개행문자 제거
+            line = line[ : -1]
 
+        newLine = Normalize(line, stopword_list).text
+        if newLine != []:
+            newText.append(newLine)
 
-text = "Hi! Welcome to my channel. I'm really excited because it's been a long time since I haven't discussed a brand and its products."
+    saveFile(f'1{fName}', newText)
 
-# 불용어 제거
-words = [word for word in text.split() if word.lower() not in stop_words]
-
-print(words)
-# 출력 결과: ['Hi!', 'Welcome', 'channel.', "I'm", 'really', 'excited', 'long', 'time', 'since', "haven't", 'discussed', 'brand', 'products.']
+    break
