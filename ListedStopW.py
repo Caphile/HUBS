79861318a275881e85124195b7d3eb35335f439a
201600284 <95951021+201600284@users.noreply.github.com>
2023-05-01 14:56:52 +0900

    ListedStopW

---
+++ ListedStopW.py
@@ -0,0 +1,14 @@
+file_path='Stopwords.txt' #Stopword.txt 파일의 단어들을 나열
+
+with open(file_path) as f:
+    lines = f.readlines()
+
+lines=[line.rstrip('\n') for line in lines]
+lines.sort()    
+print(lines)    
+
+
+words='\n'.join(lines) #배열된 단어들을 ListedStopw란 메모장에 저장되게함
+file = open("ListedStopw.txt","w")
+file.write(words)
+file.close()
