6c79c96206ef5736a473d0288de6ce9a9aa02bf2
201600284 <95951021+201600284@users.noreply.github.com>
2023-05-01 21:46:43 +0900

    Update ListedStopW.py

--- ListedStopW.py
+++ ListedStopW.py
@@ -4,8 +4,7 @@ with open(file_path) as f:
     lines = f.readlines()
 
 lines=[line.rstrip('\n') for line in lines]
-lines.sort()    
-print(lines)    
+lines.sort()      
 
 
 words='\n'.join(lines) #배열된 단어들을 ListedStopw란 메모장에 저장되게함
