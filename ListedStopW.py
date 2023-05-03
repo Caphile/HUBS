63c863cd4bbfda739fae7e62bf30ad134dbc2c86
김희찬 <caphile98@gmail.com>
2023-05-03 14:50:35 +0900

    콘솔창 색상 추가

--- ListedStopW.py
+++ ListedStopW.py
@@ -1,3 +1,6 @@
+from colorama import Fore, Style
+import os
+
 filePath = ''
 fileName = 'Stopwords.txt'
 
@@ -5,16 +8,19 @@ with open(fileName, 'r+') as f:
     lines = f.readlines()
     lines = [line.rstrip('\n') for line in lines]
     while 1:
+        print('추가/삭제할 불용어 입력 : ', end = '')
         stopW = input()
         if stopW == '':    # enter 눌러 종료
             break
 
+        os.system('cls')
+
         if stopW in lines:  # 이미 존재하면 삭제
             lines.remove(stopW)
-            print(f'{stopW}이 목록에서 삭제됨')
+            print(f'{Fore.LIGHTBLACK_EX}{stopW}{Style.RESET_ALL}이 목록에서 {Fore.RED}삭제{Style.RESET_ALL}됨')
         else:
             lines.append(stopW)
-            print(f'{stopW}이 목록에 추가됨')
+            print(f'{Fore.LIGHTBLACK_EX}{stopW}{Style.RESET_ALL}이 목록에 {Fore.BLUE}추가{Style.RESET_ALL}됨')
 
         lines.sort()    
         
