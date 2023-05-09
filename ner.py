d27b9fecddd0375e7b016186ded97ce56b022111
김희찬 <caphile98@gmail.com>
2023-05-09 19:37:17 +0900

    일부 주석 포함

--- ner.py
+++ ner.py
@@ -4,9 +4,10 @@ import spacy
 from tkinter import filedialog, Tk
 import os
 
-modelName = 'ner_model' # 추후에 직접 선택 가능하게끔 바꿀 여지 있음
-
-# hyperparam
+#-------------------------------------------------------------------------------------
+# 모델명(경로), 추후에 유동적으로 선택 가능하게끔 바꿀 여지 있음
+modelName = 'ner_model'
+# 하이퍼파라미터
 HP = {
     'dropout'   :   0.5,
     'minBatch'  :   4.0,
@@ -14,6 +15,11 @@ HP = {
     'learnRate' :   0.01,
     'epochs'    :   10
     }
+# 라벨
+labels = ['PRODUCT', 'DESCRIPTION']
+# 확률(predict)
+probability = 0.5
+#-------------------------------------------------------------------------------------
 
 def filePaths():
     root = Tk()
@@ -22,19 +28,21 @@ def filePaths():
     return filedialog.askopenfilenames(title = 'Select txt Files', initialdir = os.getcwd(), filetypes = [("Text files", "*.txt"), ("All files", "*.*")])
 
 def loadModel():
+    global modelName
     print('1. 커스텀 모델 생성')
     print('2. 기존 모델 사용')
+    print('====================================')
+    print('선택 : ', end = '')
     opt = int(input())
     if opt == 1:
         setModel()
-
+        model = spacy.load(modelName)
     elif opt == 2:
-        global modelName
         try:
-            print('커스텀 모델 사용')  # 모델을 그대로 사용할지, 튜닝 후 사용할지 결정 해야함
+            print('커스텀 모델 사용')
             model = spacy.load(modelName)
         except:
-            print('오픈소스로 제공된 학습된 모델 사용')
+            print('오픈소스로 제공된 모델 사용')
             model = spacy.load('en_core_web_sm')
     return model
 
@@ -60,11 +68,13 @@ def setModel():
 
     model = spacy.blank('en')
     model.add_pipe("ner")
-    ner = model.get_pipe("ner")
-    ner.add_label("PRODUCT")
+    ner = model.get_pipe('ner')
+    for label in labels:
+        ner.add_label(label)
+
+    model.begin_training()
 
     global HP
-    model.begin_training()
     for itn in range(HP['epochs']):
         random.shuffle(trainData)
         losses = {}
@@ -104,7 +114,7 @@ for f in fp:
                 doc = model(line)
 
                 for entity in doc.ents:
-                    if entity.label_ == "PRODUCT":
+                    if entity.label_ == 'PRODUCT':
                         prod.append(entity.text)
 
                 # 텍스트 분류를 통한 화장품 설명 추출
@@ -112,8 +122,8 @@ for f in fp:
                 sentences = [sent.text for sent in doc.sents]
                 for sentence in sentences:
                     sentence_doc = model(sentence)
-                    if sentence_doc.cats.get('DESCRIPTION', 0) > 0.5:   # 확률이 n이상인
+                    if sentence_doc.cats.get('DESCRIPTION', 0) > probability:   # 확률 조정
                         descriptions.append(sentence)
 
-        print("화장품 명:", ' '.join(prod))
-        print("화장품 설명:", ' '.join(desc))
+        print('화장품 명:', ' '.join(prod))
+        print('화장품 설명:', ' '.join(desc))
