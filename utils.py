c2999b46e54aa61f903759744e169fca80801965
김희찬 <caphile98@gmail.com>
2023-05-12 12:11:03 +0900

    공통 함수

---
+++ utils.py
@@ -0,0 +1,25 @@
+from tkinter import filedialog, Tk
+import os
+
+def filePaths():
+    root = Tk()
+    root.withdraw()
+    fullPaths = filedialog.askopenfilenames(title = 'Select txt Files', initialdir = os.getcwd(), filetypes = [("Text files", "*.txt"), ("All files", "*.*")])
+
+    paths, names = [], []
+    for p in fullPaths:
+        temp = p.split('/')
+        paths.append('/'.join(temp[ : -1]))
+        names.append(temp[-1])
+    
+    return paths, names
+
+def readFile(path, name):
+    with open(f'{path}/{name}', 'r', encoding ='UTF8') as f:
+        text = f.readlines()
+    return text
+
+def saveFile(path, name, text):
+    with open(f'{path}/{name}', 'w') as f:
+        for line in text:
+            f.write(line + '\n')
