05ae866ed6e587a683e104fd39324c72048953d9
김희찬 <caphile98@gmail.com>
2023-05-12 12:22:15 +0900

    공통함수 처리

--- ner.py
+++ ner.py
@@ -1,8 +1,8 @@
 # -*- encoding= cp949 -*-
 
 import spacy
-from tkinter import filedialog, Tk
 import os
+import utils
 
 #-------------------------------------------------------------------------------------
 # 모델명(경로), 추후에 유동적으로 선택 가능하게끔 바꿀 여지 있음
@@ -20,12 +20,6 @@ labels = ['PRODUCT', 'DESCRIPTION']
 # 확률(predict)
 probability = 0.5
 #-------------------------------------------------------------------------------------
-
-def filePaths():
-    root = Tk()
-    root.withdraw()
-
-    return filedialog.askopenfilenames(title = 'Select txt Files', initialdir = os.getcwd(), filetypes = [("Text files", "*.txt"), ("All files", "*.*")])
 
 def loadModel():
     global modelName
@@ -55,13 +49,12 @@ def setModel():
 
     trainData = []
     print('학습용 데이터 읽기')
-    fp = filePaths()
-    for f in fp:
-        with open(f, 'r', encoding ='UTF8') as f:
-            fullText = f.read()
-            lines = fullText.split('\n')
+    fp, fn = utils.filePaths()
+    for p, n in zip(fp, fn): 
+        text = utils.readFile(p, n)
+        lines = text.split('\n')
 
-            trainData.append(lines)
+        trainData.append(lines)
             
     # trainData = [("이번에 소개할 제품은 아임미미의 아이섀도우 팔레트입니다.", {"entities": [(12, 18, "ORG"), (21, 30, "PRODUCT")]})]
     # 위와 같은 형태의 trainData 제작해야 함
@@ -101,31 +94,30 @@ def setModel():
 os.system('cls')
 model = loadModel()
 print('스크립트 읽기')
-fp = filePaths()
+fp, fn = utils.filePaths()
 
 # 유튜브 스크립트에서 추출한 텍스트
-for f in fp:
-    with open(f, 'r', encoding ='UTF8') as f:
-        fullText = f.read()
-        lines = fullText.split('\n')
+for p, n in zip(fp, fn): 
+    text = utils.readFile(p, n)
+    lines = text.split('\n')
 
-        prod = []
-        desc = []
-        for line in lines:
-            if line != '':
-                doc = model(line)
+    prod = []
+    desc = []
+    for line in lines:
+        if line != '':
+            doc = model(line)
 
-                for entity in doc.ents:
-                    if entity.label_ == 'PRODUCT':
-                        prod.append(entity.text)
+            for entity in doc.ents:
+                if entity.label_ == 'PRODUCT':
+                    prod.append(entity.text)
 
-                # 텍스트 분류를 통한 화장품 설명 추출
-                descriptions = []
-                sentences = [sent.text for sent in doc.sents]
-                for sentence in sentences:
-                    sentence_doc = model(sentence)
-                    if sentence_doc.cats.get('DESCRIPTION', 0) > probability:   # 확률 조정
-                        descriptions.append(sentence)
+            # 텍스트 분류를 통한 화장품 설명 추출
+            descriptions = []
+            sentences = [sent.text for sent in doc.sents]
+            for sentence in sentences:
+                sentence_doc = model(sentence)
+                if sentence_doc.cats.get('DESCRIPTION', 0) > probability:   # 확률 조정
+                    descriptions.append(sentence)
 
-        print('화장품 명:', ' '.join(prod))
-        print('화장품 설명:', ' '.join(desc))
+    print('화장품 명:', ' '.join(prod))
+    print('화장품 설명:', ' '.join(desc))
