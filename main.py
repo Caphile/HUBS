ee8b8fb2138ea49b6b7114703c3473301fa0f86c
김희찬 <caphile98@gmail.com>
2023-05-30 19:17:26 +0900

    json 파일 생성용 뼈대 완성

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ import ner
 
 def process():
 
-	train = False
-
-	if train == True:
+	print('학습용(y/n) : ', end = '')
+	train = input()
+	if train == 'Y' or train == 'y':
 		ner.setModel()
-	elif train == False:
+	else:
 		model = ner.loadModel()
 
-		p, n = ps.crawling(True)						# crawling에 영상분석 추가 해야함
-		products = ner.extract(model, p, f'2_{n}',)[0]	# normalize된 텍스트에 대해 수행, 1개의 텍스트(임시)
+		with open(f'{p}/{n}.json', 'w') as jsonFile:
+			p, n = ps.crawling(True)						# crawling에 영상분석 추가 해야함
+			product = ner.extract(model, p, f'2_{n}',)[0]	# normalize된 텍스트에 대해 수행, 1개의 텍스트(임시)
 
-		print('\n=======================================================')
-		print('화장품 명:')
-		print('\n'.join(products))
+			print('\n=======================================================')
+			print('화장품 명:')
+			print('\n'.join(product))
+
+
+
+
+			data = {
+				'INFLUENCER'	:	1,
+				'PRODUCT'		:	product,
+				'SUMMARY'		:	1
+			}
+
+			json.dump(data, jsonFile)
 
 process()
