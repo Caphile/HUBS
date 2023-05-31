b795dc7b864ae3f1efe7346a81621c60a38c28d2
김희찬 <caphile98@gmail.com>
2023-05-31 13:57:42 +0900

    추출된 이름으로 스크립트 분량을 나누는 코드 작성

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
+from unittest.util import sorted_list_difference
 import preprocessing as ps
 import ner
+import utils
 
 def process():
 
@@ -11,23 +13,51 @@ def process():
 	else:
 		model = ner.loadModel()
 
+		p, n = ps.crawling(True)						# crawling에 영상분석 추가 해야함
 		with open(f'{p}/{n}.json', 'w') as jsonFile:
-			p, n = ps.crawling(True)						# crawling에 영상분석 추가 해야함
 			product = ner.extract(model, p, f'2_{n}',)[0]	# normalize된 텍스트에 대해 수행, 1개의 텍스트(임시)
+			product = list(set(product))
 
-			print('\n=======================================================')
-			print('화장품 명:')
-			print('\n'.join(product))
+			loc = []
+			text = utils.readFile(p, f'2_{n}')
+			for p in product:
+				for c, line in enumerate(text):
+					if p in line:
+						loc.append(c)
+						break
+
+			sortList = list(zip(product, loc))
+			sortList.sort(key = lambda x : x[1])
+
+			product, loc = zip(*sortList)
+
+			subText = []
+			for i in range(len(product)):
+				s = loc[i]
+				try:
+					e = loc[i + 1]
+					if s == e:
+						e += 1
+				except:
+					e = len(text)
+
+				subText.append(text[s : e])
+
+				print('================================================================')
+				print(product[i])
+				print(' '.join(subText[i]))
+
+				# text summerize 추가
 
 
-
-
+			
 			data = {
-				'INFLUENCER'	:	1,
+				'INFLUENCER'	:	'안녕',
 				'PRODUCT'		:	product,
-				'SUMMARY'		:	1
+				'SUMMARY'		:	subText,
 			}
 
 			json.dump(data, jsonFile)
+			
 
 process()
