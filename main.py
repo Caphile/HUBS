6f68d46103722d3fc412e06d415da1e60827de42
김희찬 <caphile98@gmail.com>
2023-06-02 12:35:23 +0900

    json 컬럼명 수정

--- main.py
+++ main.py
@@ -2,61 +2,75 @@ import json
 import preprocessing as ps
 import ner
 import utils
+import os
 
 def process():
+	while 1:	# 추후 조건 작성 예정
 
-	print('학습용(y/n) : ', end = '')
-	train = input()
-	if train == 'Y' or train == 'y':
-		ner.setModel()
-	else:
-		model = ner.loadModel()
+		print('학습용(y/n) : ', end = '')
+		train = input()
 
-		p, n = ps.crawling(True)						# crawling에 영상분석 추가 해야함
-		with open(f'{p}/{n}.json', 'w') as jsonFile:
-			product = ner.extract(model, p, f'2_{n}',)[0]	# normalize된 텍스트에 대해 수행, 1개의 텍스트(임시)
-			product = list(set(product))
+		os.system('cls')
 
-			loc = []
-			text = utils.readFile(p, f'2_{n}')
-			for p in product:
-				for c, line in enumerate(text):
-					if p in line:
-						loc.append(c)
-						break
+		if train == 'Y' or train == 'y':
+			ner.setModel()
+		else:
+			model = ner.loadModel()
 
-			sortList = list(zip(product, loc))
-			sortList.sort(key = lambda x : x[1])
+			while 1:
+				p, n = ps.crawling(True)						# crawling에 영상분석 추가 해야함
+				
+				product = ner.extract(model, p, f'2_{n}',)[0]	# normalize된 텍스트에 대해 수행, 1개의 텍스트(임시)
+				product = list(set(product))
 
-			product, loc = zip(*sortList)
+				loc = []
+				text = utils.readFile(p, f'2_{n}')
+				for pd in product:
+					for c, line in enumerate(text):
+						if pd in line:
+							loc.append(c)
+							break
 
-			subText = []
-			for i in range(len(product)):
-				s = loc[i]
-				try:
-					e = loc[i + 1]
-					if s == e:
-						e += 1
-				except:
-					e = len(text)
-
-				subText.append(text[s : e])
-
-				print('================================================================')
-				print(product[i])
-				print(' '.join(subText[i]))
-
-				# text summerize 추가
+				sortList = list(zip(product, loc))
+				sortList.sort(key = lambda x : x[1])
 
 
-			
-			data = {
-				'INFLUENCER'	:	'안녕',
-				'PRODUCT'		:	product,
-				'SUMMARY'		:	subText,
-			}
+				product, loc = zip(*sortList)
 
-			json.dump(data, jsonFile)
-			
+				subText = []
+				for i in range(len(product)):
+					s = loc[i]
+					try:
+						e = loc[i + 1]
+						if s == e:
+							e += 1
+					except:
+						e = len(text)
+
+					subText.append(text[s : e])
+
+					print('================================================================')
+					print(product[i].replace(',', ''))
+					print(' '.join(subText[i]))
+
+					# text summerize 추가
+					# summary.append() 
+
+				#print('인플루언서 입력 : ', end = '')
+				#influencer = input()
+				influencer = 'RISABAE'		# 어떻게 입력 받을지에 대한 고민 필요
+
+				product = [pd.replace(',', '') for pd in product]
+
+				with open(f'{p}/{n}.json', 'w') as jsonFile:
+
+					data = {
+						'INFL'		:	influencer,
+						'P_NM'		:	product,
+						'SUMM'		:	subText,	# summary
+						'URL'		:	text[0],
+					}
+
+					json.dump(data, jsonFile)			
 
 process()
