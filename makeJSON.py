f4c0d9e0d2a81ae0cecac0e224c08109069dcb66
김희찬 <caphile98@gmail.com>
2023-06-02 12:54:44 +0900

    임시 데이터 생성 가능하게 변경

--- makeJSON.py
+++ makeJSON.py
@@ -1,6 +1,10 @@
 import json
 import utils
 
+import random
+
+ranNM = ['A', 'B', 'C', 'D', 'E']
+
 fp, fn =  utils.filePaths(3)
 
 # YOUTUBE.json
@@ -12,8 +16,8 @@ for idx, (p, n) in enumerate(zip(fp, fn)):
 		for P_NM, SUMM in zip(SCdata['P_NM'], SCdata['SUMM']):
 			data = {
 				'P_NM'	:	P_NM,			# 제품명
-				'INFL'	:	INFL,			# 채널명
-				'SUMM'	:	SUMM,			# 요약
+				'INFL'	:	ranNM[random.randrange(0,4)],			# 채널명
+				'SUMM'	:	'SUMM',			# 요약
 				'URL'	:	URL				# url
 			}
 			YTdata[len(YTdata) + 1] = data
@@ -24,11 +28,11 @@ for i in YTdata.values():
 	P_NM = i['P_NM']
 	if P_NM not in PDdata:
 		data = {
-			'P_NM'	:	P_NM,				# 제품명
+			'PID'	:	len(PDdata) + 1,	# 제품명
 			'INGR'	:	'Ingredient',		# 성분
 			'USE'	:	'How to use',		# 사용법
 		}
-		PDdata[len(PDdata) + 1] = data
+		PDdata[P_NM] = data
 
 # INFLUENCER.json
 IFdata = {}
@@ -36,10 +40,10 @@ for i in YTdata.values():
 	INFL = i['INFL']
 	if P_NM not in IFdata:
 		data = {
-			'INFL'	:	INFL,				# 채널명
+			'IID'	:	len(IFdata) + 1,	# 채널명
 			'INFO'	:	'Infomation'		# 채널정보
 		}
-		PDdata[len(IFdata) + 1] = data
+		IFdata[INFL] = data
 
 with open('YOUTUBE.json', 'w') as f:
 	json.dump(YTdata, f)
