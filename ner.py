65d396e288efd7fc3224be7b09eaf1780d549d29
김희찬 <caphile98@gmail.com>
2023-06-07 11:29:31 +0900

    텍스트 입력으로 추출 가능하게 수정

--- ner.py
+++ ner.py
@@ -1,17 +1,18 @@
 import spacy
 from spacy.training.example import Example
 import utils
+import re
 
 #-------------------------------------------------------------------------------------
 # 모델명(경로), 추후에 유동적으로 선택 가능하게끔 바꿀 여지 있음
 modelName = 'ner_model'
 # 하이퍼파라미터
 HP = {
-    'dropout'   :   0.45,
-    'minBatch'  :   30,
-    'maxBatch'  :   40,
+    'dropout'   :   0.5,
+    'minBatch'  :   32,
+    'maxBatch'  :   50,
     'learnRate' :   0.001,
-    'epochs'    :   100,
+    'epochs'    :   150,
     'patience'  :   100
 }
 # 라벨
@@ -20,31 +21,26 @@ labels = ['PRODUCT', 'BRAND']
 #probability = 0.5
 #-------------------------------------------------------------------------------------
 
-def extract(model, fp = None, fn = None):
-    if fn == None:    # 테스트용
+def extract(model, text = None):
+    if text == None:    # 테스트용
         print('스크립트 읽기')
         fp, fn = utils.filePaths()
-    else:
-        fp, fn = [fp], [fn]
+        for p, n in zip(fp, fn): 
+            text = utils.readFile(p, n)
 
+    pattern_TS = r'\|\d+\|'
     prods = []
-    for p, n in zip(fp, fn): 
-        text = utils.readFile(p, n)
+    for line in text:
+        if line != '':
+            newLine = re.sub(pattern_TS, '', line)
+            doc = model(newLine)
+            for entity in doc.ents:
+                if entity.label_ == 'PRODUCT':
+                    prods.append(entity.text)
 
-        prod = []
-        for line in text[1 : ]:
-            if line != '':
-                doc = model(line)
-
-                for entity in doc.ents:
-                    if entity.label_ == 'PRODUCT':
-                        prod.append(entity.text)
-
-        prods.append(prod)
-
-        print('\n=======================================================')
-        print('화장품 명:')
-        print('\n'.join(prod))
+    print('\n=======================================================')
+    print('화장품 명:')
+    print('\n'.join(prods))
 
     return prods
 
@@ -83,7 +79,7 @@ def setModel():
     bestLoss = 10000
 
     print('학습 시작\n')
-    nlp.begin_training()
+    optimizer = nlp.begin_training()
     for itn in range(HP['epochs']):
         random.shuffle(trainData)
         losses = {}
@@ -98,7 +94,7 @@ def setModel():
                 doc = nlp.make_doc(text)
                 tags = spacy.training.offsets_to_biluo_tags(doc, annotation['entities'])
                 example.append(Example.from_dict(doc, {'entities': tags}))
-            nlp.update(example, drop = HP['dropout'], losses = losses)
+            nlp.update(example, drop = HP['dropout'], losses = losses, sgd = optimizer)
 
         print(f"{itn + 1} Losses: {losses['ner']:.3f}")
 
